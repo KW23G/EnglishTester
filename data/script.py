452b0800fac61088992c37537cc5ef451fe23924
JannisKohle <48388575+JannisKohle@users.noreply.github.com>
2020-04-18 10:20:09 +0200

    translated rest of data

--- data/script.py
+++ data/script.py
@@ -26,21 +26,21 @@ all_words = all_words[100:]
 if "en_to_de.json" not in os.listdir():
     os.system("touch en_to_de.json")
 
-if "de_to_de.json" not in os.listdir():
-    os.system("touch de_to_de.json")
+if "de_to_en.json" not in os.listdir():
+    os.system("touch de_to_en.json")
 
 ###################
 # en - de:
 
-# big_dict1 = {word: {"translation": trans(word), "level": 3} for word in all_words}
+big_dict1 = {word: {"translation": trans(word), "level": 3} for word in all_words}
 
-# l = int(len(big_dict1) / 4)
+l = int(len(big_dict1) / 4)
 
-# big_list1 = [create_dict(list(big_dict1.keys())[0:l], list(big_dict1.values())[0:l]), create_dict(list(big_dict1.keys())[l:(2*l)], list(big_dict1.values())[l:(2*l)]),
-    # create_dict(list(big_dict1.keys())[(2*l):(3*l)], list(big_dict1.values())[(2*l):(3*l)]), create_dict(list(big_dict1.keys())[(3*l):], list(big_dict1.values())[(3*l):])]
+big_list1 = [create_dict(list(big_dict1.keys())[0:l], list(big_dict1.values())[0:l]), create_dict(list(big_dict1.keys())[l:(2*l)], list(big_dict1.values())[l:(2*l)]),
+             create_dict(list(big_dict1.keys())[(2*l):(3*l)], list(big_dict1.values())[(2*l):(3*l)]), create_dict(list(big_dict1.keys())[(3*l):], list(big_dict1.values())[(3*l):])]
 
-# with open("en_to_de.json", "w") as f:
-    # json.dump(big_list1, f)
+with open("en_to_de.json", "w") as f:
+    json.dump(big_list1, f)
 
 ###################
 # de - en:
@@ -52,5 +52,5 @@ l = int(len(big_dict2) / 4)
 big_list2 = [create_dict(list(big_dict2.keys())[0:l], list(big_dict2.values())[0:l]), create_dict(list(big_dict2.keys())[l:(2*l)], list(big_dict2.values())[l:(2*l)]),
              create_dict(list(big_dict2.keys())[(2*l):(3*l)], list(big_dict2.values())[(2*l):(3*l)]), create_dict(list(big_dict2.keys())[(3*l):], list(big_dict2.values())[(3*l):])]
 
-with open("de_to_de.json", "w") as f:
+with open("de_to_en.json", "w") as f:
     json.dump(big_list2, f)
