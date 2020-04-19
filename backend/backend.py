c73457f4ef1208e0ae032427cda1e1b9c84ac8d7
JannisKohle <48388575+JannisKohle@users.noreply.github.com>
2020-04-19 16:16:45 +0200

    split check_translation

--- backend/backend.py
+++ backend/backend.py
@@ -8,6 +8,11 @@ def read(file):
         return json.load(f)
 
 
+def write(content, file):
+    with open(f"../data/{file}", "w") as f:
+        return json.dump(content, f)
+
+
 def create_dict(keys, values):
     return {k: v for k, v in zip(keys, values)}
 
@@ -18,15 +23,41 @@ def get_word(lan):  # lan = de / en
     pass
 
 
-def check_translation(en, de):
-    file = read("en_to_de.json")
+def check_en_to_de(en, de):
+    d = read("en_to_de.json")
 
     for x in range(4):
-        if en in file[x]:
-            if file[x][en]["translation"] == de:
-                file[x][en]["level"] -= 1
+        if en in d[x]:
+            if d[x][en]["translation"] == de:
+                d[x][en]["level"] -= 1
+                write(d, "en_to_de.json")
                 return True
 
             else:
-                file[x][en]["level"] += 1
+                d[x][en]["level"] += 1
+                write(d, "en_to_de.json")
                 return False
+
+    d[x][en]["level"] += 1
+    write(d, "en_to_de.json")
+    return False
+
+
+def check_de_to_en(de, en):
+    d = read("de_to_en.json")
+
+    for x in range(4):
+        if de in d[x]:
+            if d[x][de]["translation"] == en:
+                d[x][de]["level"] -= 1
+                write(d, "de_to_en.json")
+                return True
+
+            else:
+                d[x][de]["level"] += 1
+                write(d, "de_to_en.json")
+                return False
+
+    d[x][de]["level"] += 1
+    write(d, "de_to_en.json")
+    return False
