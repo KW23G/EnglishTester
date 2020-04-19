ce7ffdd96b2f5ce6a19645db05f2f4f1b525b322
JannisKohle <48388575+JannisKohle@users.noreply.github.com>
2020-04-19 16:20:04 +0200

    changed backend help

--- gui.py
+++ gui.py
@@ -6,10 +6,11 @@
 # How to use the backend:
 
 # from backend import backend
-# backend.check_translation(en, de) <- en = english word, de = german word
+# backend.check_en_to_de(en, de) <- en = english word, de = german word (check from en to de)
+# backend.check_de_to_en(de, en) <- en = english word, de = german word (check from de to en)
 # backend.get_word(lan) <- lan = "en" or "de" (language of output word)
 
-# backend.get_word() isn't working yet; .check_translation should work
+# backend.get_word() isn't working yet; the other functions should work
 # -> If something isn't working (althought it should), go to Issues on GitHub and add the
 # Label "Bug" to the Issue which is not working. (you can ask me how to do do that)
 
